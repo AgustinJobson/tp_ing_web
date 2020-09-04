616962a269ce7ba6a5211397a1fe3be7602afc55
DESKTOP-JP7M1VL\Cacu <francorossetti98@gmail.com>
2020-09-04 11:35:17 -0300

    requirements en la raiz

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --chdir tpingweb tpingweb.wsgi:application --log-file -
--- tpingweb/tpingweb/settings.py
+++ tpingweb/tpingweb/settings.py
@@ -43,7 +43,10 @@ INSTALLED_APPS = [
     'apps.account',
     'apps.shared',
     'apps.eventos',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
