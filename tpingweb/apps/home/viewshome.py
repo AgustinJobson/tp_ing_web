d619c34402575475bd4638627985892bf64ab38c
DESKTOP-JP7M1VL\Cacu <francorossetti98@gmail.com>
2020-10-09 23:51:35 -0300

    Search en heroku, algunas cosas en el home

--- tpingweb/apps/home/viewshome.py
+++ tpingweb/apps/home/viewshome.py
@@ -1,4 +1,30 @@
 from django.shortcuts import render
+from apps.account.models import *
+from apps.entrenamientos.models import *
+from apps.foro.models import *
+import itertools
 
 def home(request):
-    return render(request, "home.html")
+    entrenamientos = entrenamiento.objects.all()
+    entrenamientos = list(entrenamientos)
+    entrenamientos.sort(key=lambda x: x.total_likes(), reverse=True)
+    posts = Post.objects.all()
+    posts = list(posts)
+    posts.sort(key=lambda x:x.total_likes(),reverse=True)
+    top3entr = list(itertools.islice(entrenamientos, 3))
+    top3posts = list(itertools.islice(posts, 3))
+    entrenadores = []
+    print (top3entr)
+    comuns = User.objects.all()
+    for c in comuns:
+        if c.groups.filter(name = 'entrenador').exists():
+            entrenadores.append(c)
+
+
+    entrenadores = set(entrenadores)
+    context = {
+        'entrenamientos':top3entr,
+        'entrenadores': entrenadores,
+        'posts': top3posts,
+    }
+    return render(request, "home.html", context)
